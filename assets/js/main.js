c9ea8c1a19bb850858d9fff52277719e973452fa
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-01 12:23:53 -0500

    ternaria result

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 // var resultado = resultado de todas las operaciones
 
-var continuar = prompt("¿Quiere realizar otra operación");
-var finalizar = continuar == "Sí" ? alert("Seleccione el tipo de operación") : resultado
+var respuestaUser = prompt("¿Quiere realizar otra operación");
+var finalizar = respuestaUser == "Sí" ? alert("Seleccione el tipo de operación") : result
