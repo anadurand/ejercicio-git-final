5b582805ed1ef09d1c33a8d2789ec3953e1de7d5
Geraldine Chauca <gchaucae@gmail.com>
2017-03-01 13:21:52 -0500

    Intento de fusión. Bota error.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,50 @@
 var name = document.getElementById('name');
-var num1 = document.getElementById('num1');
+var num1 = document.getElementById('num1').value;
+var num2 = document.getElementById("num2").value;
+var operacion = document.getElementById("operacion");
+var opcion = operacion.options[operacion.selectedIndex].value;
+var result;
+console.log(opcion);
 
-function sumar (num1, num2){
+function operar(num1, num2, opcion){
+
+  switch (opcion) {
+
+    case 1:
+              result = suma(num1, num2);
+      break;
+
+    case 2:
+              result = resta(num1, num2);
+        break;
+
+    case 3:
+              result = multiplicar(num1,num2);
+      break;
+
+    case 4:
+              result = dividir(num1, num2);
+      break;
+    case 5:
+              result = potencia(num1, num2);
+      break;
+    case 6:
+              result = raiz(num1, num2);
+      break;
+    default:
+            result =  "Error";
+  }
+
+  document.getElementById("salida").innerHTML = result;
+}
+
+function suma (num1, num2){
   return num1 + num2;
 }
 
 function multiplicar(num1, num2){
   return num1 * num2;
 }
+
+
 
