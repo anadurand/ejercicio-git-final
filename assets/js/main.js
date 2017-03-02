56bf78ea66cc8d3cba6aba11e303aa04080d055c
Usuario <Usuario>
2017-03-01 23:36:17 -0500

    casi terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 var result = 0;
+//Para prevenir que se refresque la pagina cada vez que se pulsa el boton
 document.getElementById("form").onsubmit=function(e){
   e.preventDefault();
 }
+
+//funcion que llama a las operaciones dependiendo del valor seleccionado
 function operar(num1, num2){
 
   var name = document.getElementById('name');
@@ -38,8 +41,10 @@ function operar(num1, num2){
             result =  "Error";
   }
 
-  document.getElementById("salida").innerHTML = result;
+
+  preguntar();
 }
+
 
 function suma (num1, num2){
   return (num1 + num2);
@@ -62,4 +67,27 @@ function potencia (num1, num2){
 }
 function raiz (num1, num2){
   return Math.sqrt(num1, num2);
+}
+
+function preguntar(){
+  var pregunta = prompt("Desea continuar operando?: \n 0 = No \n 1 = Si");
+  if(pregunta == "1"){
+    alert("Ingrese numero 2 y operacion en el formulario");
+    document.getElementById("num1").value = result;
+    document.getElementById("num2").value = "";
+    document.getElementById("operacion").value = "";
+  }else if (pregunta == "0") {
+
+    document.getElementById("salida").innerHTML = result;
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("operacion").value = "";
+  }else{
+    alert("Valor Invalido, operacion terminada");
+    document.getElementById("salida").innerHTML = result;
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("operacion").value = "";
+
+  }
 }
